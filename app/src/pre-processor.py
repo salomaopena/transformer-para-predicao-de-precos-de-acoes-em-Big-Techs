47e0f336ad418964e07f15498338a67a4cb7aa89
Gabriel França de Almeida <54248551+GabrielFAlmeida@users.noreply.github.com>
2025-05-27 00:15:20 -0300

    feat(Jupyter Notebooks): criar notebooks do Jupyter e adicionar funções

---
+++ app/src/pre-processor.py
@@ -0,0 +1,11 @@
+# Pre processor file
+# Authors: Gabriel França de Almeida e Eneia Gazite
+# Start Date: 2025-05-26
+# Last Update: 2025-05-26
+
+# Import section
+import pandas as pd
+import numpy as np
+
+# Code section
+def cut_DataFrame_by_Period(df: pd.DataFrame) -> pd.DataFrame:
